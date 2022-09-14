319e5f972c25562e5a2180b41a57225349bbe801
AlexandeOliveira12 <lebella321@gmail.com>
2022-09-13 23:49:13 -0300

    Inserir Dados No Mysql

---
+++ README.md
@@ -0,0 +1,46 @@
+# INSERINDO REGISTROS USANDO INTERFACE GRAFICA
+
+## INSIRA REGISTROS NO BANCO DE DADOS MYSQL USANDO INTERFACE GRAFICA COM A LIB DO TKINTER
+
+VERSÃO=2.0  
+DB's COMPATIVEIS = MYSQL
+
+Para baixar o repositório escreva no terminal(cmd):
+````
+git clone https://github.com/AlexandeOliveira12/Searching-Records-Database
+````
+Para baixar os requirements escreva no terminal(cmd):
+````
+pip install -r requirements.txt
+````
+Dependencias necessarias para usar este recuros:
+Python;
+Mysql;
+Tkinter;
+
+(Obs: Substitua as informações de conexão do servidor pelas suas!)
+
+
+##
+
+# INSERTING RECORDS USING GRAPHIC INTERFACE
+
+## INSERT RECORDS INTO MYSQL DATABASE USING GRAPHIC INTERFACE WITH TKINTER LIB
+
+VERSION=2.0
+COMPATIBLE DB's = MYSQL
+
+To download the repository write in the terminal(cmd):
+````
+git clone https://github.com/AlexandeOliveira12/Searching-Records-Database
+````
+To download the requirements write in the terminal(cmd):
+````
+pip install -r requirements.txt
+````
+Dependencies needed to use this feature:
+Python;
+mysql;
+Tkinter;
+
+(Note: Replace the server connection information with your own!)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mysql.connector
 from tkinter import *
 from decouple import config
 
-def inserir():
+def InserirDados():
     myconnection = mysql.connector.connect(host=config("localhost"), user='root', password=config("password"), database=config("database"))
 
     i = int(idade.get())
@@ -20,25 +20,25 @@ def inserir():
     if myconnection.is_connected(): 
         cursor.close()
         myconnection.close()
-        print("Conexao ao MySql foi encerrada")
-        
+        print("Conexao ao MySql foi encerrada")      
         
 janela = Tk()
-janela.title("query tools")
+janela.geometry("240x150")
+janela.title("Login")
 
 Nome1 = Label(janela, text="insira seu nome aqui aqui: ")
 Nome1.grid(column=0, row=0)
 
-Nome = Entry(janela, width=50)
+Nome = Entry(janela, width=40)
 Nome.grid(column=0, row=1)
 
-idade1 = Label(janela, text="insira sua idade aqui:")
+idade1 = Label(janela, text="\ninsira sua idade aqui:")
 idade1.grid(column=0, row=2)
 
-idade = Entry(janela, width=50)
+idade = Entry(janela, width=40)
 idade.grid(column=0, row=3)
 
-botao = Button(janela, text="Inserir dados", command=inserir)
+botao = Button(janela, text="Inserir dados", command=lambda:[InserirDados()])
 botao.grid(column=0, row=4)
 
 janela.mainloop()        
