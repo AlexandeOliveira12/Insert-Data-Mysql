fb0fa3fefb4956566511d225609083dec6491f51
AlexandeOliveira12 <lebella321@gmail.com>
2022-09-30 01:10:22 -0300

    Atualização 2.0

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from tkinter import *
 from tkinter import messagebox
 from decouple import config
 
+    
+
+#Função de inserção de dados
 def InserirDados():
     myconnection = mysql.connector.connect(host=config("localhost"), user='root', password=config("password"), database=config("database"))
-
+     
     i = int(idade.get())
-    n = str(Nome.get())    
+    n = str(Nome.get()) 
 
     MsgBox = messagebox.askquestion ('Salvar dados','Deseja salvar esses dados?',icon = 'question')
 
     if MsgBox == 'yes':
-       janela.destroy()
+       JanelaLogin.destroy()
         
     cursor = myconnection.cursor()
     sql = "INSERT INTO logins(nome, idade) VALUE (%s, %s)"
@@ -28,23 +31,37 @@ def InserirDados():
         myconnection.close()
         print("Conexao ao MySql foi encerrada")      
         
-janela = Tk()
-janela.geometry("240x150")
-janela.title("Login")
+     
+      
+        
+def Janela2():
+    janelaConta = Tk()
+    janelaConta.geometry("240x150")
+    janelaConta.title("Conta")
+    
+    i = int(idade.get())
+    n = str(Nome.get())
+    
+    Saudacao = Label(janelaConta, text="Seja bem vindo" + n + i)
+    Saudacao.grid(column=0, row=0)
+    
+JanelaLogin = Tk()
+JanelaLogin.geometry("240x150")
+JanelaLogin.title("Login")
 
-Nome1 = Label(janela, text="insira seu nome aqui aqui: ")
+Nome1 = Label(JanelaLogin, text="insira seu nome aqui aqui: ")
 Nome1.grid(column=0, row=0)
 
-Nome = Entry(janela, width=40)
+Nome = Entry(JanelaLogin, width=40)
 Nome.grid(column=0, row=1)
 
-idade1 = Label(janela, text="\ninsira sua idade aqui:")
+idade1 = Label(JanelaLogin, text="\ninsira sua idade aqui:")
 idade1.grid(column=0, row=2)
 
-idade = Entry(janela, width=40)
+idade = Entry(JanelaLogin, width=40)
 idade.grid(column=0, row=3)
 
-botao = Button(janela, text="Inserir dados", command=lambda:[InserirDados()])
+botao = Button(JanelaLogin, text="Inserir dados", command=lambda:[InserirDados(), Janela2()])
 botao.grid(column=0, row=4)
 
-janela.mainloop()        
+JanelaLogin.mainloop()        
